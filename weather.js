8a29c338b424ecd73f3d93bc0532e926debd138b
Boyan Xu <developerxu123@gmail.com>
2020-02-17 20:11:36 -0700

    forcast done

--- weather.js
+++ weather.js
@@ -2,14 +2,14 @@ $(document).ready(function () {
     $('#run-search').on("click", function () {
         event.preventDefault();
         var input = $("#search-value").val();
-        var city = input[0].toUpperCase()+input.slice(1);
+        var city = input[0].toUpperCase() + input.slice(1);
         var history = [];
         history.push(city);
         localStorage.setItem("history", JSON.stringify(history));
         var parseHistory = JSON.parse(localStorage.getItem("history")) || [];
         parseHistory.forEach(element => {
-           var li = $("<li>").text(element)
-           $(".historyli").append(li);
+            var li = $("<li>").text(element)
+            $(".historyli").append(li);
         });
 
         var lat;
@@ -24,9 +24,78 @@ $(document).ready(function () {
             }).then(function (data) {
                 var uv = data.value;
                 console.log(uv);
+                
             });
         }
+        function forcast(city) {
+            var fcURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=c10bb3bd22f90d636baa008b1529ee25&units=imperial"
+            $.ajax({
+                url: fcURL,
+                method: "GET"
+            }).then(function (fcData) {
+                console.log(fcData);
+                var fc1 = fcData.list[4].dt_txt.slice(0, 10);
+                var fc2 = fcData.list[12].dt_txt.slice(0, 10);
+                var fc3 = fcData.list[20].dt_txt.slice(0, 10);
+                var fc4 = fcData.list[28].dt_txt.slice(0, 10);
+                var fc5 = fcData.list[36].dt_txt.slice(0, 10);
+                var $fc1Div = $("<div>");
+                var $fc2Div = $("<div>");
+                var $fc3Div = $("<div>");
+                var $fc4Div = $("<div>");
+                var $fc5Div = $("<div>");
 
+                var fc1temp = fcData.list[4].main.temp;
+                var fc2temp = fcData.list[12].main.temp;
+                var fc3temp = fcData.list[20].main.temp;
+                var fc4temp = fcData.list[28].main.temp;
+                var fc5temp = fcData.list[36].main.temp;
+
+                var fc1hmdy = fcData.list[4].main.humidity;
+                var fc2hmdy = fcData.list[12].main.humidity;
+                var fc3hmdy = fcData.list[20].main.humidity;
+                var fc4hmdy = fcData.list[28].main.humidity;
+                var fc5hmdy = fcData.list[36].main.humidity;
+
+                var $fc1 = $("<h6>").text(fc1);
+                var $fc2 = $("<h6>").text(fc2);
+                var $fc3 = $("<h6>").text(fc3);
+                var $fc4 = $("<h6>").text(fc4);
+                var $fc5 = $("<h6>").text(fc5);
+
+                var $fc1temp = $("<p>").text("Temp: " + fc1temp + " °F");
+                var $fc2temp = $("<p>").text("Temp: " + fc2temp + " °F");
+                var $fc3temp = $("<p>").text("Temp: " + fc3temp + " °F");
+                var $fc4temp = $("<p>").text("Temp: " + fc4temp + " °F");
+                var $fc5temp = $("<p>").text("Temp: " + fc5temp + " °F");
+
+                var $fc1hmdy = $("<p>").text("Humidity: " + fc1hmdy + "%");
+                var $fc2hmdy = $("<p>").text("Humidity: " + fc2hmdy + "%");
+                var $fc3hmdy = $("<p>").text("Humidity: " + fc3hmdy + "%");
+                var $fc4hmdy = $("<p>").text("Humidity: " + fc4hmdy + "%");
+                var $fc5hmdy = $("<p>").text("Humidity: " + fc5hmdy + "%");
+                $fc1Div.append($fc1);
+                $fc2Div.append($fc2);
+                $fc3Div.append($fc3);
+                $fc4Div.append($fc4);
+                $fc5Div.append($fc5);
+                $fc1Div.append($fc1temp);
+                $fc2Div.append($fc2temp);
+                $fc3Div.append($fc3temp);
+                $fc4Div.append($fc4temp);
+                $fc5Div.append($fc5temp);
+                $fc1Div.append($fc1hmdy);
+                $fc2Div.append($fc2hmdy);
+                $fc3Div.append($fc3hmdy);
+                $fc4Div.append($fc4hmdy);
+                $fc5Div.append($fc5hmdy);
+                $("#square1").html($fc1Div);
+                $("#square2").html($fc2Div);
+                $("#square3").html($fc3Div);
+                $("#square4").html($fc4Div);
+                $("#square5").html($fc5Div);
+            });
+        }
 
         if (city !== '') {
             $.ajax({
@@ -57,6 +126,7 @@ $(document).ready(function () {
 
                 $("#weather-view").html($weatherDiv);
                 getUV(lat, lon);
+                forcast(city);
             });
         }
         else {
